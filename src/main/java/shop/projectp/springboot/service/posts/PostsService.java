68dfcad06ef73b189decf9e7b15e6872211bdbde
yang <kiki9510@gmail.com>
2021-05-04 01:51:14 +0900

    Added update, read by id functions.

--- src/main/java/shop/projectp/springboot/service/posts/PostsService.java
+++ src/main/java/shop/projectp/springboot/service/posts/PostsService.java
@@ -3,8 +3,11 @@ package shop.projectp.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.projectp.springboot.domain.posts.Posts;
 import shop.projectp.springboot.domain.posts.PostsRepository;
+import shop.projectp.springboot.web.dto.PostsResponseDto;
 import shop.projectp.springboot.web.dto.PostsSaveRequestDto;
+import shop.projectp.springboot.web.dto.PostsUpdateRequestDto;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +17,22 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
     }
 }

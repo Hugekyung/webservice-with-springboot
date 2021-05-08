890892c4c3114fd97ea23c2984d973a47e9f9b2b
yang <kiki9510@gmail.com>
2021-05-08 13:13:49 +0900

    Added BaseTimeEntity for Auditing function and applied to whole project.

--- src/main/java/shop/projectp/springboot/domain/BaseTimeEntity.java
+++ src/main/java/shop/projectp/springboot/domain/BaseTimeEntity.java
@@ -10,13 +10,13 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass
+@MappedSuperclass // BaseTimeEntity를 상속할 경우 아래 필드들도 칼럼으로 인식하게 한다.
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
+    @CreatedDate // Entity 생성, 저장 시의 시간이 자동저장된다.
     private LocalDateTime createdDate;
 
-    @LastModifiedDate
+    @LastModifiedDate // Entity의 값을 변경할 때의 시간이 자동저장된다.
     private LocalDateTime modifiedDate;
 }

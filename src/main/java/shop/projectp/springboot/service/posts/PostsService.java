9343cbfcb6b655248d69ef871395021f6b6c6fb7
yang <kiki9510@gmail.com>
2021-05-16 00:46:44 +0900

    Added posts save, update functions.

--- src/main/java/shop/projectp/springboot/service/posts/PostsService.java
+++ src/main/java/shop/projectp/springboot/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.projectp.springboot.domain.posts.Posts;
 import shop.projectp.springboot.domain.posts.PostsRepository;
+import shop.projectp.springboot.web.dto.PostsListResponseDto;
 import shop.projectp.springboot.web.dto.PostsResponseDto;
 import shop.projectp.springboot.web.dto.PostsSaveRequestDto;
 import shop.projectp.springboot.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 등록. 수정. 삭제 기능이 없는 순수 조회만 가능할 때 사용하면 조회 속도가 개선된다.
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/shop/projectp/springboot/web/IndexController.java
+++ src/main/java/shop/projectp/springboot/web/IndexController.java
@@ -1,18 +1,35 @@
 package shop.projectp.springboot.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import shop.projectp.springboot.service.posts.PostsService;
+import shop.projectp.springboot.web.dto.PostsResponseDto;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
-        return "index"; // 머스테치 파일의 이름으로 파일 경로를 자동 생성한다.
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
     }
 
     @GetMapping("/posts/save")
     public String postsSave() {
-        return "posts-save";
+        return "posts-save"; // 머스테치 파일의 이름으로 파일 경로를 자동 생성한다.
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }

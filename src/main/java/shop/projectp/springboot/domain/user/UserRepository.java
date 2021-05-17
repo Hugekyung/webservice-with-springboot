fab659e4c3a229e98dabc8c968b3d33d43d89416
yang <kiki9510@gmail.com>
2021-05-18 01:10:24 +0900

    Added Oauth security function.

---
+++ src/main/java/shop/projectp/springboot/domain/user/UserRepository.java
@@ -0,0 +1,11 @@
+package shop.projectp.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email); // email 정보로 기존에 가입한 정보가 있는지 확인한다.
+
+}
